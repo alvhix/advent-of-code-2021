6e8f05779fd6023b3cff42bb493924731e807e92
Alvhix <alvhix@gmail.com>
2022-02-13 17:43:34 +0100

    Day 03 - Part 2

--- day-03/day-03.py
+++ day-03/day-03.py
@@ -30,6 +30,71 @@ def binary_to_decimal(binary):
     return decimal
 
 
+# part 2
+def calculate_oxygen_rating(arr):
+    for i in range(0, len(arr[0])):
+        # count zeros and ones
+        zeros = 0
+        ones = 0
+        for element in arr:
+            if element[i] == "0":
+                zeros += 1
+            elif element[i] == "1":
+                ones += 1
+
+        # filter array by the most common bit
+        if zeros > ones:
+            arr = filter_by_bit(arr, "0", i)
+        else:
+            arr = filter_by_bit(arr, "1", i)
+
+        # if the array has only one element, stop, we got the number
+        if len(arr) == 1:
+            break
+
+    return arr[0]
+
+
+def calculate_co2_rating(arr):
+    for i in range(0, len(arr[0])):
+        # count zeros and ones
+        zeros = 0
+        ones = 0
+        for element in arr:
+            if len(arr) == 1:
+                break
+            if element[i] == "0":
+                zeros += 1
+            elif element[i] == "1":
+                ones += 1
+
+        # filter array by the least common bit
+        if zeros <= ones:
+            arr = filter_by_bit(arr, "0", i)
+        else:
+            arr = filter_by_bit(arr, "1", i)
+
+        # if the array has only one element, stop, we got the number
+        if len(arr) == 1:
+            break
+
+    return arr[0]
+
+
+def filter_by_bit(arr, bit, digit):
+    result = []
+    for element in arr:
+        if element[digit] == bit:
+            result.append(element)
+    return result
+
+
+def calculate_support_rating(arr):
+    return binary_to_decimal(calculate_oxygen_rating(arr)) * binary_to_decimal(
+        calculate_co2_rating(arr)
+    )
+
+
 # setup
 file = open("input.txt", "r")
 # list of strings
@@ -37,3 +102,4 @@ arr = file.read().split("\n")
 file.close()
 
 print(calculate_power_consumption(arr))
+print(calculate_support_rating(arr))
